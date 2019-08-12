7f3a131c21356c777d3bfe60a59963cd251eceee
mtmtkzm <hello@mtmtkzm.com>
2019-08-12 22:34:05 +0900

    Add env の渡すkeyが間違えていたので修正

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: process.env.FB_DATABASE_URL,
-    authDomain: process.env.FB_DATABASE_URL,
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
     databaseURL: process.env.FB_DATABASE_URL,
-    projectId: process.env.FB_DATABASE_URL,
-    storageBucket: process.env.FB_DATABASE_URL,
-    messagingSenderId: process.env.FB_DATABASE_URL,
-    appId: process.env.FB_DATABASE_URL
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+    appId: process.env.FB_APP_ID
   })
 }
 
